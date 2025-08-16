a2ed77d35ab4e0c7ebe10518a77d6d9a2f93416b
Ahmad Shohazar <ahmadshohazar2911@gmail.com>
2025-08-16 13:57:36 +0700

    chg config

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
 // Konfigurasi mesin fingerprint
-$ip = "192.168.0.102"; // IP mesin
+$ip = "192.168.0.100"; // IP mesin
 $port = 80;  
 $soap_port = $port;
 $key = 0;              // Communication key
